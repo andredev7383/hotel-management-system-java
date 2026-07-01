54ec52b939558a3c86c459a99edbbc64be078574
André Fernandes <nobunaga7k@gmail.com>
2026-07-01 14:35:00 -0300

    Hospede.java

---
+++ Hospede.java
@@ -0,0 +1,23 @@
+public class Hospede {
+
+    private String nome;
+    private String cpf;
+
+    public Hospede(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + " | CPF: " + cpf;
+    }
+}
